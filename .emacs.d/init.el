f8b41712a56e0e27477b1fbc4abaf0a97351382d
Vladimir Ivanov <lester.mail@gmail.com>
2018-04-16 00:37:16 -0400

    Add init.el

---
+++ .emacs.d/init.el
@@ -0,0 +1,104 @@
+;; global variables
+(setq
+ inhibit-startup-screen t
+ create-lockfiles nil
+ make-backup-files nil
+ column-number-mode t
+ scroll-error-top-bottom t
+ show-paren-delay 0.5
+ use-package-always-ensure t
+ sentence-end-double-space nil)
+
+;; buffer local variables
+(setq-default
+ indent-tabs-mode nil
+ tab-width 4
+ c-basic-offset 4)
+
+;; modes
+(electric-indent-mode 0)
+
+;; global keybindings
+(global-unset-key (kbd "C-z"))
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
+
+;; the package manager
+(require 'package)
+(setq
+ package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                    ("org" . "http://orgmode.org/elpa/")
+                    ("melpa" . "http://melpa.org/packages/")
+                    ("melpa-stable" . "http://stable.melpa.org/packages/"))
+ package-archive-priorities '(("melpa-stable" . 1)))
+
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+;;(ido-mode 1)
+;;(setq ido-everywhere t)
+;;(setq ido-enable-flex-matching t)
+(use-package flx-ido
+  :demand
+  :init
+  (setq
+   ido-enable-flex-matching t
+   ;; C-d to open directories
+   ;; C-f to revert to find-file
+   ido-show-dot-for-dired nil
+   ido-enable-dot-prefix t)
+  :config
+  (ido-mode 1)
+  (ido-everywhere 1)
+  (flx-ido-mode 1))
+(use-package idris-mode)
+(use-package ensime
+  :ensure t
+  :pin melpa)
+(use-package sbt-mode
+  :pin melpa)
+(use-package scala-mode
+  :pin melpa)
+(use-package magit
+  :commands magit-status magit-blame
+  :init (setq
+         magit-revert-buffers nil)
+  :bind (("s-g" . magit-status)
+         ("s-b" . magit-blame))
+  :pin melpa)
+(use-package git-timemachine)
+(add-hook 'git-timemachine-mode-hook (lambda () (ensime-mode 0)))
+(use-package git-gutter
+  :config
+  (custom-set-variables
+   '(git-gutter:modified-sign " * ")
+   '(git-gutter:added-sign    " + ")
+   '(git-gutter:deleted-sign " - ")))
+(global-git-gutter-mode +1)
+(use-package projectile
+  :demand
+  :init   (setq projectile-use-git-grep t)
+  :config (projectile-global-mode t)
+  :bind   (("s-f" . projectile-find-file)
+           ("s-F" . projectile-grep)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (tango-dark)))
+ '(git-gutter:added-sign " + ")
+ '(git-gutter:deleted-sign " - ")
+ '(git-gutter:modified-sign " * ")
+ '(package-selected-packages
+   (quote
+    (smerge git-gutter git-timemachine flx-ido magit ensime idris-mode use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(set-frame-font "PragmataPro")
